f077dc5712a3458bf5a1ffac071f82e162e2e9f6
ruxia2015 <ruxia2010@126.com>
2015-09-23 17:25:10 +0800

    添加yahoo搜索

--- siteServer/src/main/java/com/dong/sitserver/util/HttpUtils.java
+++ siteServer/src/main/java/com/dong/sitserver/util/HttpUtils.java
@@ -4,6 +4,8 @@ package com.dong.sitserver.util;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
@@ -36,12 +38,17 @@ public class HttpUtils {
             httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
             //让服务器知道访问源为浏览器
             httpClient.getParams().setParameter(HttpMethodParams.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; rv:8.0.1) Gecko/20100101 Firefox/8.0.1");
-
             List<Header> headers = new ArrayList<Header>();
             headers.add(new Header("Cookie", "lang=CH; load_balancer=dcd47a65-d505-4de2-a521-d9ca7145da5a; JSESSIONID=981453C8B9516E0DFCA183C1F7F15E0C.node16"));
             headers.add(new Header("Connection", "keep-alive"));
             httpClient.getHostConfiguration().getParams().setParameter(
                     "http.default-headers", headers);
+
+
+          //  httpClient.getHostConfiguration().setProxy("192.168.101.1", 5608);
+           //  httpClient.getParams().setAuthenticationPreemptive(true);
+            //如果代理需要密码验证，这里设置用户名密码
+            // httpClient.getState().setProxyCredentials(AuthScope.ANY, new UsernamePasswordCredentials("llying.iteye.com","llying"));
 
 
             GetMethod method = new GetMethod(url);
